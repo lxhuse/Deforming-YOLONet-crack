993a26e69558869fa0744e8271d19e3e5040c475
UltralyticsAssistant <web@ultralytics.com>
2025-12-01 05:21:33 +0000

    Auto-format by https://ultralytics.com/actions

--- demo.py
+++ demo.py
@@ -1,4 +1,6 @@
 from ultralytics import YOLO
 
 if __name__ == "__main__":
-    model = YOLO("ultralytics/cfg/models/v8/yolov8-seg-DSC.yaml").train(data="DSC_demo.yaml", epochs=100, imgsz=512, batch=4, save_period=10)
+    model = YOLO("ultralytics/cfg/models/v8/yolov8-seg-DSC.yaml").train(
+        data="DSC_demo.yaml", epochs=100, imgsz=512, batch=4, save_period=10
+    )
